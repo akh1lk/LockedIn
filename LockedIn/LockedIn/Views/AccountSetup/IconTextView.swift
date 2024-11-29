133c09ea878b83c4a42a7ce33e6d4ef4dd5961da
Gabriel <gac232@cornell.edu>
2024-11-29 18:00:35 -0500

    Finish modular MultiChoiceView

---
+++ LockedIn/LockedIn/Views/AccountSetup/IconTextView.swift
@@ -0,0 +1,115 @@
+//
+//  IconTextView.swift
+//  LockedIn
+//
+//  Created by Gabriel Castillo on 11/29/24.
+//
+
+import UIKit
+
+protocol IconTextViewDelegate {
+    func didTap(_: IconTextView)
+}
+
+/// Custom view that behaves as a button that has an icon and text.
+class IconTextView: UIView {
+    // MARK: - UI Components
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 20
+        view.clipsToBounds = true
+        view.layer.borderColor = UIColor.palette.purple.cgColor
+        view.layer.borderWidth = 2.5
+        return view
+    }()
+    
+    private let textLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .palette.offBlack
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.text = "Lorem Ipsum"
+        return label
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.tintColor = .palette.offBlack
+        iv.image = UIImage(systemName: "questionmark")
+        return iv
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.tintColor = .clear
+        button.layer.cornerRadius = 25
+        button.backgroundColor = .clear
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Data
+    public var delegate: IconTextViewDelegate?
+    public let data: IconTextOption
+    
+    // MARK: - Life Cycle
+    init(with option: IconTextOption) {
+        data = option
+        
+        super.init(frame: .zero)
+        
+        iconImageView.image = option.icon
+        textLabel.text = option.text
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI Setup
+    private func setupUI() {
+        self.backgroundColor = .clear
+        
+        self.addSubview(backgroundView)
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(iconImageView)
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(textLabel)
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            iconImageView.centerYAnchor.constraint(equalTo: backgroundView.centerYAnchor),
+            iconImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10),
+            
+            iconImageView.widthAnchor.constraint(equalToConstant: 20),
+            iconImageView.heightAnchor.constraint(equalToConstant: 20),
+            
+            textLabel.centerYAnchor.constraint(equalTo: iconImageView.centerYAnchor, constant: -0.5),
+            textLabel.leadingAnchor.constraint(equalTo: iconImageView.trailingAnchor, constant: 5),
+            
+            backgroundView.topAnchor.constraint(equalTo: self.topAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: textLabel.trailingAnchor, constant: 15),
+            backgroundView.heightAnchor.constraint(equalToConstant: 40),
+        ])
+    }
+    
+    var totalWidth: CGFloat {
+        let textLabelWidth = textLabel.intrinsicContentSize.width
+        let totalWidth = 20 + 5 + textLabelWidth
+        return totalWidth
+    }
+    
+    
+    // MARK: - Selectors
+    @objc private func buttonTapped() {
+        print("noob")
+        delegate?.didTap(self)
+    }
+}
---
+++ LockedIn/LockedIn/Views/AccountSetup/MultiChoiceView.swift
@@ -0,0 +1,94 @@
+//
+//  MultiChoiceView.swift
+//  LockedIn
+//
+//  Created by Gabriel Castillo on 11/29/24.
+//
+
+import UIKit
+
+/// A view presenting multiple options in a semi random order.
+class MultiChoiceView: UIView {
+    // MARK: - UI Components
+    
+    
+    // MARK: - Data
+    private var allOptions: [IconTextOption] = []
+    private var limit: Int
+    private var screenWidth: CGFloat
+    public var selectedOptions: [IconTextOption] = []
+    
+    // MARK: - Life Cycle
+    /// Creates a MultiChoiceView
+    ///
+    ///
+    /// - Parameters:
+    ///     - options: the list of IconTextOption objects that you'd like to be shown
+    ///     - limit: the maximum number of options that can be selected.
+    ///     - screenWidth: the width of the screen.
+    ///
+    init(options: [IconTextOption], limit: Int, screenWidth: CGFloat) {
+        self.allOptions = options
+        self.limit = limit
+        self.screenWidth = screenWidth
+        
+        super.init(frame: .zero)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - UI Setup
+    
+    func setupUI() {
+        // Semi-randomly formats options.
+        var currentTopAnchor = self.topAnchor
+        let padding: CGFloat = 15.0
+        var viewsWidth: CGFloat = 0
+        var numViews = 3
+        
+        var index = 0
+        
+        while index < allOptions.count {
+            numViews = numViews == 3 ? 2 : 3
+            
+            let containerView = UIView()
+            self.addSubview(containerView)
+            containerView.translatesAutoresizingMaskIntoConstraints = false
+            
+            var trailingAnchor = containerView.leadingAnchor
+            
+            for _ in 1...numViews {
+                let view = IconTextView(with: allOptions[index])
+                containerView.addSubview(view)
+                view.translatesAutoresizingMaskIntoConstraints = false
+                
+                view.backgroundColor = .green
+                NSLayoutConstraint.activate([
+                    view.widthAnchor.constraint(equalToConstant: view.totalWidth),
+                    view.leadingAnchor.constraint(equalTo: trailingAnchor, constant: padding + 20),
+                ])
+                  
+                trailingAnchor = view.trailingAnchor
+                viewsWidth += view.totalWidth
+                index += 1
+            }
+            
+            NSLayoutConstraint.activate([
+                containerView.heightAnchor.constraint(equalToConstant: 40),
+                containerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+                containerView.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: -20),
+                containerView.topAnchor.constraint(equalTo: currentTopAnchor, constant: padding),
+            ])
+            currentTopAnchor = containerView.bottomAnchor
+        }
+    }
+    
+    
+    // MARK: - Selectors
+    
+}
