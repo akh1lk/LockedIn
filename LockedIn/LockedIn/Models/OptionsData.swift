f703bbd0e841faa6ab76827f29eede2e4ca59c82
Gabriel <gac232@cornell.edu>
2024-11-29 18:43:48 -0500

    finsih multiChoiceView with data

---
+++ LockedIn/LockedIn/Models/OptionsData.swift
@@ -0,0 +1,25 @@
+//
+//  OptionsData.swift
+//  LockedIn
+//
+//  Created by Gabriel Castillo on 11/29/24.
+//
+
+import UIKit
+
+struct OptionsData {
+    static let careerGoalOptions: [IconTextOption] = [
+        IconTextOption(icon: UIImage(systemName: "dollarsign.circle.fill"), text: "Internships"),
+        IconTextOption(icon: UIImage(systemName: "network"), text: "Networking"),
+        IconTextOption(icon: UIImage(systemName: "briefcase.fill"), text: "Full-Time Work"),
+        IconTextOption(icon: UIImage(systemName: "graduationcap.fill"), text: "Mentee"),
+        IconTextOption(icon: UIImage(systemName: "book.fill"), text: "Mentor"),
+        IconTextOption(icon: UIImage(systemName: "person.2.fill"), text: "Partnerships"),
+        IconTextOption(icon: UIImage(systemName: "play.fill"), text: "Start-ups"),
+        IconTextOption(icon: UIImage(systemName: "star.fill"), text: "For Fun"),
+        IconTextOption(icon: UIImage(systemName: "brain.fill"), text: "Upskilling"),
+        IconTextOption(icon: UIImage(systemName: "magnifyingglass.circle.fill"), text: "Research"),
+        IconTextOption(icon: UIImage(systemName: "heart.fill"), text: "Volunteering"),
+        IconTextOption(icon: UIImage(systemName: "video.fill"), text: "Content-Creation"),
+    ]
+}
