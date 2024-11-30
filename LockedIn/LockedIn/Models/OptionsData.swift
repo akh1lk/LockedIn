f4bbcc2f24bfaba8740b51343609a4677c9cb520
Gabriel <gac232@cornell.edu>
2024-11-30 02:19:35 -0500

    Finish first two screens for account setup

--- LockedIn/LockedIn/Models/OptionsData.swift
+++ LockedIn/LockedIn/Models/OptionsData.swift
@@ -22,4 +22,27 @@ struct OptionsData {
         IconTextOption(icon: UIImage(systemName: "heart.fill"), text: "Volunteering"),
         IconTextOption(icon: UIImage(systemName: "video.fill"), text: "Content-Creation"),
     ]
+    
+    static let interestsOptions: [IconTextOption] = [
+        IconTextOption(icon: UIImage(systemName: "figure.run"), text: "Running"),
+        IconTextOption(icon: UIImage(systemName: "figure.dance"), text: "Dance"),
+        IconTextOption(icon: UIImage(systemName: "bicycle"), text: "Biking"),
+        IconTextOption(icon: UIImage(systemName: "camera.fill"), text: "Photography"),
+        IconTextOption(icon: UIImage(systemName: "figure.hiking"), text: "Hiking"),
+        IconTextOption(icon: UIImage(systemName: "figure.surfing"), text: "Surfing"),
+        IconTextOption(icon: UIImage(systemName: "pencil.and.outline"), text: "Drawing"),
+        IconTextOption(icon: UIImage(systemName: "figure.pool.swim"), text: "Swimming"),
+        IconTextOption(icon: UIImage(systemName: "paintbrush.fill"), text: "Painting"),
+        IconTextOption(icon: UIImage(systemName: "tennis.racket"), text: "Tennis"),
+        IconTextOption(icon: UIImage(systemName: "basketball.fill"), text: "Basketball"),
+        IconTextOption(icon: UIImage(systemName: "pencil"), text: "Sewing"),
+        IconTextOption(icon: UIImage(systemName: "figure.bowling"), text: "Bowling"),
+        IconTextOption(icon: UIImage(systemName: "tshirt.fill"), text: "Knitting"),
+        IconTextOption(icon: UIImage(systemName: "book.pages.fill"), text: "Reading"),
+        IconTextOption(icon: UIImage(systemName: "gamecontroller.fill"), text: "Video Gaming"),
+        IconTextOption(icon: UIImage(systemName: "tent.fill"), text: "Camping"),
+        IconTextOption(icon: UIImage(systemName: "music.note.list"), text: "Music"),
+        IconTextOption(icon: UIImage(systemName: "frying.pan.fill"), text: "Cooking"),
+        IconTextOption(icon: UIImage(systemName: "figure.yoga"), text: "Yoga"),
+    ]
 }
