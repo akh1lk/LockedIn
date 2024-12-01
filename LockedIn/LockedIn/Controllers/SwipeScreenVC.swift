5f6777366591a828ae8d20aae0e178a367cf906d
Gabriel <gac232@cornell.edu>
2024-11-30 22:26:54 -0500

    Finish Location access view controller

--- LockedIn/LockedIn/Controllers/SwipeScreenVC.swift
+++ LockedIn/LockedIn/Controllers/SwipeScreenVC.swift
@@ -10,11 +10,26 @@ import UIKit
 /// The screen where users swipe on people they want to network with.
 class SwipeScreenVC: UIViewController {
 
+    // MARK: - UI Components
+    
+    // MARK: - Data
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        // TODO: Add location checking functionality here!
+        let vc = LocationAccessVC()
+        vc.isModalInPresentation = true
+        let nav = UINavigationController(rootViewController: vc)
+        self.present(nav, animated: true, completion: nil)
+        
+        
     }
-
-
+    
+    // MARK: - UI Setup
+    
+    // MARK: - Selectors
 }
 
