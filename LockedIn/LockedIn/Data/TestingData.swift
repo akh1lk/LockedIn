bff9ecb757c4c20f1f3536c262a4c89bbb4a513c
Gabriel <gac232@cornell.edu>
2024-12-03 18:29:34 -0500

    This chat view looks like dick

--- LockedIn/LockedIn/Data/TestingData.swift
+++ LockedIn/LockedIn/Data/TestingData.swift
@@ -10,8 +10,12 @@ import UIKit
 struct TestingData {
     
     static let messages = [
-        Message(sender: Sender(avatar: UIImage(named: "ye"), senderId: "user1", displayName: "John Pork"), messageId: "1", sentDate: Date(), kind: .text("Hello there!")),
-        Message(sender: Sender(avatar: UIImage(named: "diddy"), senderId: "user2", displayName: "Jane Doe"), messageId: "2", sentDate: Date().addingTimeInterval(-3600), kind: .text("Hi, how can I help you?")),
+        Message(sender: Sender(avatar: UIImage(named: "diddy"), senderId: "user2", displayName: "Jane Doe"), messageId: "1", sentDate: Date().addingTimeInterval(-3800), kind: .text("Hi, how can I help you?")),
+        Message(sender: Sender(avatar: UIImage(named: "diddy"), senderId: "user2", displayName: "Jane Doe"), messageId: "2", sentDate: Date().addingTimeInterval(-3600), kind: .text(" bot?")),
+        Message(sender: Sender(avatar: UIImage(named: "diddy"), senderId: "user2", displayName: "Jane Doe"), messageId: "3", sentDate: Date().addingTimeInterval(-3500), kind: .text(" nope")),
+        Message(sender: Sender(avatar: UIImage(named: "diddy"), senderId: "user2", displayName: "Jane Doe"), messageId: "4", sentDate: Date().addingTimeInterval(-3000), kind: .text(" gau")),
+        Message(sender: Sender(avatar: UIImage(named: "diddy"), senderId: "user2", displayName: "Jane Doe"), messageId: "5", sentDate: Date().addingTimeInterval(-2900), kind: .text("sup")),
+        
     ]
     
     static let users = [
