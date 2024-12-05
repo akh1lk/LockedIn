63ac2dd355e71a720b3c2c40b44b742e3a51a0ca
Gabriel <gac232@cornell.edu>
2024-12-05 03:20:02 -0500

    Commit before making more info view

--- LockedIn/LockedIn/Controllers/ChatTableVC.swift
+++ LockedIn/LockedIn/Controllers/ChatTableVC.swift
@@ -11,6 +11,12 @@ import MessageKit
 class ChatTableVC: UIViewController {
     
     // MARK: - UI Components
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .palette.offWhite
+        return view
+    }()
+    
     private let tableView: UITableView = {
         let tableView = UITableView()
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -60,8 +66,16 @@ class ChatTableVC: UIViewController {
         self.view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         
+        self.view.addSubview(backgroundView)
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            backgroundView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: self.view.topAnchor, constant: 110),
+            backgroundView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            
+            tableView.topAnchor.constraint(equalTo: backgroundView.bottomAnchor),
             tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: -20),
             tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
